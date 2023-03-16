1266b8d3b0d8153533ea81178e53d4bef89f546a
Andrey K. Tsoy <a.choi@smartsoltech.kr>
2023-03-16 18:37:45 +0900

    DB models created. Mirated

--- main/catalog/admin.py
+++ main/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Supplier, Customer, Payment, Product, Order, Review, Cart#,Category
 # Register your models here.
+
+admin.site.register(Supplier)
+admin.site.register(Customer)
+admin.site.register(Payment)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(Review)
--- main/catalog/models.py
+++ main/catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class MyModelName(models.Model):
     """
     A typical class defining a model, derived from the Model class.
@@ -27,3 +27,69 @@ class MyModelName(models.Model):
         return self.field_name
 
 my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
+
+from django.db import models
+
+
+#Модели таблиц
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=[
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('SHIPPED', 'Shipped'),
+        ('DELIVERED', 'Delivered'),
+        ('CANCELLED', 'Cancelled'),
+    ], max_length=20)
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(choices=[
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('CONFIRMATION', 'Confirmation'),
+        ('ACCEPTED', 'Accepted'),
+        ('CANCELLED', 'Cancelled'),
+    ], max_length=20)
+    date_paid = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    date_added = models.DateTimeField(auto_now_add=True)
+
